f560149d8aa85baa35df0173f4396431d7b21185
Erik Raith <raithhtiar@gmail.com>
2020-05-21 19:34:54 -0600

    [fix] Change server info and start script

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ app.use(express.static(__dirname+'/dist'));
 
 app.listen(process.env.PORT || 8080);
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
 // PathLocationStrategy
 
 app.get('/*', function(req,res) {
