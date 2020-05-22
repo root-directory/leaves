54d4cefd5f831c8dec6bb43a016d0ac765226d83
Erik Raith <raithhtiar@gmail.com>
2020-05-21 19:50:21 -0600

    [fix] server

--- server.js
+++ server.js
@@ -2,29 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static('./dist/leaves'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/leaves/'}
+);
+});
 
 app.listen(process.env.PORT || 8080);
-
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
-
-// PathLocationStrategy
-
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname + 'dist/index.html'));
-})
-
-console.log('Console Listening!');
