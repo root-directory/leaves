3eef4e231afc88bc6ca97ac02bc09ae067a7e89d
Erik Raith <raithhtiar@gmail.com>
2020-05-24 12:27:41 -0600

    [feat] Add routes scaffolding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ForestComponent } from './forest/forest.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, 
+  { path: 'forest', component: ForestComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/forest/forest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forest',
+  templateUrl: './forest.component.html',
+  styleUrls: ['./forest.component.scss']
+})
+export class ForestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
