a5befc2accbfcca7c257aec0d888f5394c23da9c
Erik Raith <raithhtiar@gmail.com>
2020-05-24 15:16:04 -0600

    [feat][style] Add forest basic outline
    Co-authored-by: Karen <knguyen4184@yahoo.com>

--- src/app/forest/forest.component.ts
+++ src/app/forest/forest.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {PLANTS} from '../mock-plants';
 @Component({
   selector: 'app-forest',
   templateUrl: './forest.component.html',
   styleUrls: ['./forest.component.scss']
 })
 export class ForestComponent implements OnInit {
-
+  plants = PLANTS;
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/mock-plants.ts
@@ -0,0 +1,8 @@
+import { Plant } from './plant';
+
+export const PLANTS: Plant[] = [
+  { id: 1, name: 'Mater', plantType:'Tomato' },
+  { id: 2, name: 'Pliskin', plantType:'Snake' },
+  { id: 3, name: 'Nemo', plantType:'Goldfish' },
+  { id: 4, name: 'Hugo', plantType:'Spider' },
+];
