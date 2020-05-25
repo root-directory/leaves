1aedc85667bd42542859cb158067fd4ef17b6e28
Erik Raith <raithhtiar@gmail.com>
2020-05-25 16:58:58 -0600

    [feat][wip] Add HTTP server to project
    Co-authored-by: Karen <knguyen4184@yahoo.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ForestComponent } from './forest/forest.component';
 import { HomeComponent } from './home/home.component';
@@ -24,7 +29,11 @@ import { PlantNewComponent } from './plant-new/plant-new.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
