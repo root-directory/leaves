649bb81f26c8f57d5b9061b07b00b9ee8832a9b5
Erik Raith <raithhtiar@gmail.com>
2020-07-15 13:44:44 -0600

    [reafactor] Remove logic from component.

--- src/app/forest/forest.component.ts
+++ src/app/forest/forest.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { PlantService } from '../../services/plant.service';
 import { Plant } from '../types/plant';
-import { Store, select } from '@ngrx/store';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import * as PlantActions from '../../Rx/plants.actions';
 import * as fromRoot from '../../Rx/rx.index';
@@ -16,7 +15,6 @@ export class ForestComponent implements OnInit {
   plants$: Observable<Plant[]>;
 
   constructor(
-    private plantService: PlantService,
     private store: Store<fromRoot.State>,
     private titleService: TitleService
   ) {
@@ -30,43 +28,5 @@ export class ForestComponent implements OnInit {
 
   delete(plant: Plant): void {
     this.store.dispatch(PlantActions.deletePlant({ plant }));
-  }
-
-  lastWatered(plant) {
-    const datesDiff = Date.now() - plant.lastWatered;
-    const daysDiff = Math.floor(datesDiff / (1000 * 60 * 60 * 24));
-    let alert = {
-      color: 'white',
-      title: '',
-      lastWatered: '',
-      daysUntil: '',
-    };
-    const wateringFrequencyDays = plant.care.watering.frequency * 7;
-    console.log(wateringFrequencyDays);
-    if (!wateringFrequencyDays) {
-      alert = {
-        title: `No watering events in your journal!`,
-        lastWatered: `Add a new Journal Event`,
-        daysUntil: `and be sure to update your plant care with intervals`,
-        color: 'green',
-      };
-    } else if (daysDiff > wateringFrequencyDays) {
-      alert = {
-        title: `Your Plant is Thirsty!`,
-        lastWatered: `Last Watered:${daysDiff} days ago. `,
-        daysUntil: ` Past Due by: ${daysDiff - wateringFrequencyDays}days!`,
-        color: 'red',
-      };
-    } else {
-      alert = {
-        title: `Nice Watering!`,
-        lastWatered: `Last Watered:${daysDiff} days ago. `,
-        daysUntil: ` ${
-          wateringFrequencyDays - daysDiff
-        }days, until you need to water this plant!`,
-        color: 'green',
-      };
-    }
-    return { daysSinceLastWatered: daysDiff, alert };
   }
 }
